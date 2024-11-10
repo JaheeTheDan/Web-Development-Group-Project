e0dbea64ca0c87126781e8a95037400148e113da
JaheeTheDan <jaheemshaw2@outlook.com>
2024-11-10 18:02:08 -0500

    Add login_regist.js to project and remove login.js name

--- app/login_regist.js
+++ app/login_regist.js
@@ -1,6 +1,52 @@
 // Jaheem Shaw 2205642
+
+/**
+ * This function is responsible for registering a new user by collecting user data from the signup form,
+ * validating the input, adding the user to the list of registered users, and storing the updated list in local storage.
+ * After successful registration, the user is redirected to the home page.
+ *
+ * @param {HTMLElement} formElement - The HTML form element containing the signup form data.
+ * @returns {void}
+ */
+function registerNewUser(formElement) {
+    // Get the signup form data
+    const sign_up_form = new FormData(formElement);
+
+    // Convert the FormData object to a plain JavaScript object for easier manipulation
+    formDataObj ={};
+    sign_up_form.forEach((value, key) => (formDataObj[key] = value));
+    console.log(formDataObj);
+
+    // Retrieve the list of registered users from local storage
+    let list_of_users = JSON.parse(localStorage.getItem("list_of_register_users"));
+
+    // Add the new user to the list with an empty cart
+    list_of_users[username] = {"cart" : []};
+
+    // Copy the form data to the user object in the list
+    for ([key, value] of sign_up_form){
+        list_of_users[username][key]=value;
+    };
+
+    // Display a success message to the user
+    alert("Account made successfully.");
+
+    // Store the updated list of registered users in local storage
+    localStorage.setItem("list_of_register_users", JSON.stringify(list_of_users));
+
+    // Set the login status and the currently logged-in user in local storage
+    localStorage.setItem("is_a_user_login", true);
+    localStorage.setItem("login_user", JSON.stringify(list_of_users[username]));
+
+    // Redirect the user to the home page
+    window.location.href = "index.html";
+}
+
 const login_btn = document.getElementById("login_btn");
 const sign_up_btn = document.getElementById("sign_up_btn");
+
+
+registerNewUser();
 
 login_btn.addEventListener("click", function(event) {
     let try_login_count = 0;
